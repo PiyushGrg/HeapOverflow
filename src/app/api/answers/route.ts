41b9a616c7d64d114d3d627a8cb28328fb93b459
Pgarg2025 <97442147+Pgarg2025@users.noreply.github.com>
2024-03-16 12:21:24 +0530

    Email feature

--- src/app/api/answers/route.ts
+++ src/app/api/answers/route.ts
@@ -4,8 +4,13 @@ import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@clerk/nextjs/server";
 import { getMongoUseridFromClerkId } from "@/actions/users";
 import Question from "@/models/QuestionModel";
+import { Resend } from "resend";
+import User from "@/models/userModel";
+import EmailTemplate from "@/components/EmailTemplate";
 
 connectDB();
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request:NextRequest) {
     try {
@@ -19,7 +24,26 @@ export async function POST(request:NextRequest) {
         question.totalAnswers += 1;
         await question.save();
 
-        return NextResponse.json({message:"Answer posted successfully"},{status:201});
+        const questionOwner = await User.findById(question.user);
+        const email:string = questionOwner!.email;
+
+        // author details
+        const author = await User.findById(reqBody.user);
+        const authorName = author!.name;
+        const authorImage = author!.profilePicUrl as string;
+        const reviewText = reqBody.description.length > 100 ? reqBody.description.substring(0, 100) + '...' : reqBody.description;
+
+        // send notification to question owner
+        const data = await resend.emails.send({
+            from: 'Heap Overflow <no-reply@heapoverflow.tech>',
+            to: email,
+            subject: '🎉 Your question has been answered!',
+            react: EmailTemplate({ authorName, authorImage, reviewText,questionId:question._id}),
+        });
+
+        // console.log(data);
+
+        return NextResponse.json(data);
         
     } catch (error:any) {
         return NextResponse.json({message:error.message},{status:500});
---
+++ src/components/EmailTemplate.tsx
@@ -0,0 +1,172 @@
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Hr,
+  Html,
+  Img,
+  Link,
+  Preview,
+  Row,
+  Section,
+  Text,
+} from "@react-email/components";
+import * as React from "react";
+import logo from "../../public/logo.png";
+
+interface AirbnbReviewEmailProps {
+  authorName: string;
+  authorImage: string;
+  reviewText: string;
+  questionId: string;
+}
+
+const baseUrl = `https://${process.env.VERCEL_URL}`
+
+const AirbnbReviewEmail = ({
+  authorName,
+  authorImage,
+  reviewText,
+  questionId,
+}: AirbnbReviewEmailProps) => {
+
+  const previewText = `Read ${authorName}'s answer`;
+
+  return (
+    <Html>
+      <Head />
+      <Preview>{previewText}</Preview>
+
+      <Body style={main}>
+        <Container style={container}>
+          <Section>
+            <Img
+              src={`${baseUrl}/static/logo.png`}
+              width="96"
+              height="30"
+              alt="HeapOverflow"
+            />
+          </Section>
+          <Section>
+            <Img
+              src={authorImage}
+              width="96"
+              height="96"
+              alt={authorName}
+              style={userImage}
+            />
+          </Section>
+          <Section style={{ paddingBottom: "20px" }}>
+            <Row>
+              <Text style={heading}>Here's what {authorName} answered</Text>
+              <Text style={review}>{reviewText}</Text>
+              <Text style={paragraph}>
+                You can read the full answer by clicking the button below. You can also leave a comment for {authorName} if you'd like.
+              </Text>
+              <Text style={{ ...paragraph, paddingBottom: "16px" }}>
+                Thanks for using HeapOverflow!
+              </Text>
+
+              <Button style={button} href={`https://heapoverflow.tech/questions/viewQuestion/${questionId}`}>
+                View Answer
+              </Button>
+            </Row>
+          </Section>
+
+          <Hr style={hr} />
+
+          <Section>
+            <Row>
+              <Text style={footer}>
+                This email was sent to you as a registered HeapOverflow user, HeapOverflow Inc.
+              </Text>
+              <Text style={footer}>
+                Logo Credits : <Link style={reportLink} href="https://www.linkedin.com/in/ankit-bhalla-9b6965223/">Ankit Bhalla</Link>
+              </Text>
+            </Row>
+          </Section>
+        </Container>
+      </Body>
+    </Html>
+  );
+};
+
+
+export default AirbnbReviewEmail;
+
+const main = {
+  backgroundColor: "#ffffff",
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
+};
+
+const container = {
+  margin: "0 auto",
+  padding: "20px 0 48px",
+  width: "580px",
+  maxWidth: "100%",
+};
+
+const userImage = {
+  margin: "0 auto",
+  marginBottom: "16px",
+  borderRadius: "50%",
+};
+
+const heading = {
+  fontSize: "32px",
+  lineHeight: "1.3",
+  fontWeight: "700",
+  color: "#484848",
+};
+
+const paragraph = {
+  fontSize: "18px",
+  lineHeight: "1.4",
+  color: "#484848",
+};
+
+const review = {
+  ...paragraph,
+  padding: "24px",
+  backgroundColor: "#f2f3f3",
+  borderRadius: "4px",
+};
+
+const button = {
+  backgroundColor: "#FEA500",
+  borderRadius: "3px",
+  color: "#5D127C",
+  fontSize: "18px",
+  paddingTop: "19px",
+  paddingBottom: "19px",
+  textDecoration: "none",
+  textAlign: "center" as const,
+  display: "block",
+  width: "100%",
+};
+
+const link = {
+  ...paragraph,
+  color: "#5D127C",
+  display: "block",
+};
+
+const reportLink = {
+  fontSize: "14px",
+  color: "#9ca299",
+  textDecoration: "underline",
+};
+
+const hr = {
+  borderColor: "#cccccc",
+  margin: "20px 0",
+};
+
+const footer = {
+  color: "#9ca299",
+  fontSize: "14px",
+  marginBottom: "10px",
+};
+
